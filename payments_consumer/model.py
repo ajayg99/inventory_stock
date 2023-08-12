30463e5e40aa69851c6e96eca9c2efbdb80a0d45
ajayg99 <g.ajaygg@outlook.com>
2023-08-12 18:23:57 -0400

    automated

--- payments_consumer/model.py
+++ payments_consumer/model.py
@@ -2,9 +2,8 @@ from redis_om import get_redis_connection, HashModel
 
 
 redis_payment = get_redis_connection(
-  host='redis-11804.c256.us-east-1-2.ec2.cloud.redislabs.com',
-  port=11804,
-  password="e58YcAo5M67OvWd7UZabr7o60XADlUBy"
+  host='redis-inventory-service',
+  port=80
 )
 class Order(HashModel):
     product_id: str
